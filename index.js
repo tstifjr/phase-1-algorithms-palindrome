71023c9b889e650cf3394d77ddf4b4e9828f0136
TJ Stifter <tjstifter@gmail.com>
2023-06-02 14:08:19 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  wordArray = Array.from(word);
+  arrayLength = wordArray.length;
+  for (let i = 0; i < Math.floor(arrayLength / 2); i++) {
+    if (wordArray[i] !== wordArray[(arrayLength - 1) - i]) return false;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  create array of the word
+  create length of array
+  write for loop to compare each element of first half to each of second half and if any are not equal return false
+  after comparing all elements in for loop return true if no false occurs
 */
 
 /*
   Add written explanation of your solution here
+  i created an array from the word string, and also created a variable for the arraylength.  Then i used a for loop to compare each element in the first half 
+  to each element in the second half reversed (e.g. first to last, second to second to last).
+  for loop only runs through half the array b/c that will confirm palindrome.  Used math.floor to round down uneven lengths since the middle char doesn't need to be compared
+  (ie. will always be in same spot)
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +34,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  // my test code.
+  // const arrayNames = ['matiam', 'america', 'mannam', 'manam', 'abolishy']
+  // arrayNames.forEach(name => {
+  //   console.log(name, isPalindrome(name));
+  // })
 }
 
 module.exports = isPalindrome;
